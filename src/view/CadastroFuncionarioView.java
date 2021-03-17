6ed5d82b16f77305093f2c5cde456e9500d27a73
Leo <leo@gmail.com>
2021-03-17 16:30:16 -0300

    Adicionado CadastroFuncionarioView

---
+++ src/view/CadastroFuncionarioView.java
@@ -0,0 +1,31 @@
+package view;
+
+import java.awt.Image;
+
+import javax.swing.*;
+
+public class CadastroFuncionarioView {
+	private JLabel labelNome;
+	private JTextArea inputNome;
+	private JLabel labelCPF;
+	private JTextArea inputCPF;
+	private JLabel labelUsuario;
+	private JTextArea inputUsuario;
+	private JLabel labelSenha;
+	private JPasswordField inputSenha;
+	private JLabel labelCargo;
+	private JComboBox inputCargo;
+	private JButton btnSalvar;
+	private JButton btnCancelar;
+	private JPanel painelPrincipal;
+	private Image igmFoto;
+	
+	public void CadastroFuncinarioSalvar() {
+		
+	}
+	
+	public void CadastroFuncionarioCancelar() {
+		
+	}
+
+}
