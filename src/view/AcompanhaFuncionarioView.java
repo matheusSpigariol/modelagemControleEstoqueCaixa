3b67716c520bb3b0547193e339b697c7c2233d5f
Leo <leo@gmail.com>
2021-03-17 16:40:59 -0300

    Adicionado AcompanhaFuncionarioView

---
+++ src/view/AcompanhaFuncionarioView.java
@@ -0,0 +1,31 @@
+package view;
+
+import java.awt.Image;
+
+import javax.swing.*;
+
+import model.Funcionario;
+
+public class AcompanhaFuncionarioView {
+	private JTable tabelaFuncionarios;
+	private JButton btnEditar;
+	private JButton btnExcluir;
+	private JLabel labelPesquisa;
+	private JTextArea inputPesquisa;
+	private JPanel painelPrincipal;
+	private Image imgFoto;
+	
+	public void EditarFuncionario() {
+		
+	}
+	
+	public void ExcluirFuncionario() {
+		
+	}
+	
+	public Funcionario PesquisarFuncionario()
+	{
+		return null;
+		
+	}
+}
