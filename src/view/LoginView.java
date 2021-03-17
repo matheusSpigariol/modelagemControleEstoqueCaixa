3b5a30c0f0ba9b1f4caf6614e590286a660aeb1c
matheusSpigariol <contato.matheused@gmail.com>
2021-03-17 16:13:21 -0300

    adicionado LoginView

---
+++ src/view/LoginView.java
@@ -0,0 +1,20 @@
+package view;
+
+import java.awt.Image;
+import javax.swing.*;  
+
+public class LoginView {
+	private JLabel labelLogin;
+	private JTextArea inputLogin;
+	private JLabel labelSenha;
+	private JTextArea inputSenha;
+	private JButton btnLogin;
+	private JPanel painelPrincipal;
+	private Image imgFoto;
+	
+	public void Login() {
+		
+	}
+	
+
+}
