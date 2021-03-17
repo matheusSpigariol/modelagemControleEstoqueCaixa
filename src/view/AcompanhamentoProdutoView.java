1ae13494cefd71299a5f20177381d7c47f528488
raphamsfonseca <raphamsfonseca@hotmail.com>
2021-03-17 17:09:49 -0300

    Adicionado acompanhamento de produto

---
+++ src/view/AcompanhamentoProdutoView.java
@@ -0,0 +1,32 @@
+package view;
+
+import java.awt.Image;
+
+import javax.swing.*;
+
+import model.Produto;
+
+public class AcompanhamentoProdutoView {
+	private JTable tabelaProdutos; 
+	private JButton btnEditar;
+	private JButton btnExcluir;
+	private JButton btnGerarRelatorio;
+	private JLabel labelPesquisa;
+	private JTextArea inputPesquisa;
+	private JPanel painelPrincipal;
+	private Image imgFoto;
+	
+	public void EditarProduto() {
+		
+	}
+	public void ExcluirProduto() {
+		
+	}
+	public Produto PesquisarProduto() {
+		return null;
+	}
+	public void GerarRelatorio() {
+		
+	}
+	
+}
